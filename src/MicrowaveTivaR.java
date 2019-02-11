7f698b27bcb317442bddc38e2947388042d6352e
TivaR8 <tiva.rait@ocsbstudent.ca>
2019-02-11 13:10:56 -0500

    Everything but taken screenshots

--- src/MicrowaveTivaR.java
+++ src/MicrowaveTivaR.java
@@ -8,7 +8,33 @@ import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+/*
+ * Created by: Tiva Rait
+ * Created on: 11-Feb-2019
+ * Created for: ICS4U
+ * Daily Assignment – Day #6 Microwave
+ * This program Calculates the amount of time a food...
+ * ... show be heated up in the microwave based on how...
+ * ... many of the same food have been placed in the microwave.
+*/
 public class MicrowaveTivaR {
+	
+	// Put all of the Global variables here.
+			static double TWOITEMS = 0.5;
+			static double THREEITEMS = 1;
+			
+			// All in seconds of how long it should be in for
+			static double SUBTIME = 60;
+			static double PIZZATIME = 45;
+			static double SOUPTIME = 135;
+			
+			// Booleans
+			boolean subSelected = false;
+			boolean pizzaSelected = false;
+			boolean soupSelected = false;
+			boolean oneItemSelected = false;
+			boolean twoItemsSelected = false;
+			boolean threeItemsSelected = false;
 
 	private JFrame frame;
 
@@ -33,22 +59,6 @@ public class MicrowaveTivaR {
 	 */
 	public MicrowaveTivaR() {
 		initialize();
-		// Put all of the Global variables here.
-		double TWOITEMS = 0.5;
-		double THREEITEMS = 1;
-		
-		// All in seconds of how long it should be in for
-		double SUBTIME = 60;
-		double PIZZATIME = 45;
-		double SOUPTIME = 135;
-		
-		// Booleans
-		boolean subSelected = false;
-		boolean pizzaSelected = false;
-		boolean soupSelected = false;
-		boolean oneItemSelected = false;
-		boolean twoItemsSelected = false;
-		boolean threeItemsSelected = false;
 		
 	}
 
@@ -62,10 +72,24 @@ public class MicrowaveTivaR {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		JLabel lblAnswer = new JLabel("=");
+		lblAnswer.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAnswer.setBounds(10, 173, 354, 14);
+		frame.getContentPane().add(lblAnswer);
+		
+		
 		JButton btnSub = new JButton("Sub");
 		btnSub.addActionListener(new ActionListener() {
 			// Sub
 			public void actionPerformed(ActionEvent arg0) {
+				// Booleans
+				subSelected = true;
+				pizzaSelected = false;
+				soupSelected = false;
+				
+				// Make sure answer text says nothing at first
+				lblAnswer.setText(" ");
+				
 			}
 		});
 		btnSub.setBounds(10, 36, 89, 23);
@@ -75,6 +99,12 @@ public class MicrowaveTivaR {
 		btnPizza.addActionListener(new ActionListener() {
 			// Pizza
 			public void actionPerformed(ActionEvent e) {
+				subSelected = false;
+				pizzaSelected = true;
+				soupSelected = false;
+				
+				// Make sure answer text says nothing at first
+				lblAnswer.setText(" ");
 			}
 		});
 		btnPizza.setBounds(143, 36, 89, 23);
@@ -84,6 +114,12 @@ public class MicrowaveTivaR {
 		btnSoup.addActionListener(new ActionListener() {
 			// Soup
 			public void actionPerformed(ActionEvent e) {
+				subSelected = false;
+				pizzaSelected = false;
+				soupSelected = true;
+				
+				// Make sure answer text says nothing at first
+				lblAnswer.setText(" ");
 			}
 		});
 		btnSoup.setBounds(275, 36, 89, 23);
@@ -103,6 +139,32 @@ public class MicrowaveTivaR {
 		btnOneItem.addActionListener(new ActionListener() {
 			// One Item
 			public void actionPerformed(ActionEvent e) {
+				
+				// Local answer variable
+				double answer = 1;
+				
+				// Determine which food is being placed in
+				if (subSelected == true)
+				{
+					answer = SUBTIME;
+				}
+				else if (pizzaSelected == true)
+				{
+					answer = PIZZATIME;
+				}
+				else if (soupSelected == true)
+				{
+					answer = SOUPTIME;
+				}
+				else
+				{
+					lblAnswer.setText("Please select a food");
+					
+				}
+				
+				//Give the answer 
+				lblAnswer.setText("Your food should be placed in for "+ answer);
+					
 			}
 		});
 		btnOneItem.setBounds(10, 113, 89, 23);
@@ -112,6 +174,31 @@ public class MicrowaveTivaR {
 		btnTwoItems.addActionListener(new ActionListener() {
 			// Two Items
 			public void actionPerformed(ActionEvent e) {
+				
+				// Local answer variable
+				double answer = 1;
+				
+				// Determine which food is being placed in and calculate
+				if (subSelected == true)
+				{
+					answer = ((TWOITEMS * SUBTIME) + SUBTIME);
+				}
+				else if (pizzaSelected == true)
+				{
+					answer = ((TWOITEMS * PIZZATIME) + PIZZATIME);
+				}
+				else if (soupSelected == true)
+				{
+					answer = ((TWOITEMS * SOUPTIME) + SOUPTIME);
+				}
+				else
+				{
+					lblAnswer.setText("Please select a food");
+					
+				}
+				
+				//Give the answer 
+				lblAnswer.setText("Your food should be placed in for "+ answer);
 			}
 		});
 		btnTwoItems.setBounds(143, 113, 89, 23);
@@ -121,14 +208,37 @@ public class MicrowaveTivaR {
 		btnThreeItems.addActionListener(new ActionListener() {
 			// Three Items
 			public void actionPerformed(ActionEvent e) {
+				
+				// Local answer variable
+				double answer = 1;
+				
+				// Determine which food is being placed in and calculate
+				if (subSelected == true)
+				{
+					answer = ((THREEITEMS * SUBTIME) + SUBTIME);
+				}
+				else if (pizzaSelected == true)
+				{
+					answer = ((THREEITEMS * PIZZATIME) + PIZZATIME);
+				}
+				else if (soupSelected == true)
+				{
+					answer = ((THREEITEMS * SOUPTIME) + SOUPTIME);
+				}
+				else
+				{
+					lblAnswer.setText("Please select a food");
+					
+				}
+				
+				//Give the answer 
+				lblAnswer.setText("Your food should be placed in for "+ answer);
+				
 			}
 		});
 		btnThreeItems.setBounds(275, 113, 89, 23);
 		frame.getContentPane().add(btnThreeItems);
 		
-		JLabel lblAnswer = new JLabel("=");
-		lblAnswer.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAnswer.setBounds(10, 173, 354, 14);
-		frame.getContentPane().add(lblAnswer);
+		
 	}
 }
